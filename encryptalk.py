20b223e137b43ea7cd654c8ee5caa72c640f05c7
kimjw0427 <63496362+kimjw0427@users.noreply.github.com>
2021-07-22 13:51:11 +0900

    Update encryptalk.py

--- encryptalk.py
+++ encryptalk.py
@@ -3,6 +3,112 @@ import socket
 import time
 # -*- coding: utf-8 -*-
 from PyQt5 import QtCore, QtWidgets, QtGui
+import random
+
+ML_A = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987, 1993, 1997, 1999]
+
+S_D = 0
+S_N = 0
+
+C_E = 0
+C_N = 0
+
+def Q_mod(x, d, n):
+    y = x % n
+    r = 1
+    bd = bin(d)
+    bd = bd[2:len(bd)]
+    m = len(bd) - 1
+    while (m != -1):
+        if bd[m] == '1':
+            r = r * y % n
+        y = y ** 2 % n
+        m = m - 1
+    return r
+
+
+def ML_test(n, a):
+    d = (n - 1) // 2
+    while (d % 2 == 0):
+        if (pow(a, d, n) == n - 1):
+            return True
+        d = d // 2
+    tmp = pow(a, d, n)
+    if tmp == n - 1 or tmp == 1:
+        return True
+    else:
+        return False
+
+
+def F_test(n):
+    a = 3
+    if Q_mod(a, n - 1, n) == 1 or n == 3:
+        return True
+    else:
+        return False
+
+
+def prime_test(n):
+    result = True
+    if F_test(n):
+        for x in ML_A:
+            if not ML_test(n, x):
+                result = False
+                break
+            if result:
+                return True
+    else:
+        return False
+
+
+def extended_euclid(r1, r2, s1=1, s2=0, t1=0, t2=1):
+    # 처음 시작 시 s1 ~ t2 초기값 설정
+    q = r1 // r2  # 두 수의 몫
+    s = s1 - s2 * q  # 첫 번째 항의 곱
+    t = t1 - t2 * q  # 두 번째 항의 곱
+    r = r1 - r2 * q  # 두 수의 나머지와 같다.
+    if (r == 0):
+        res = {'gcd': r2, 's': s2, 't': t2}
+        return res
+    else:
+        return extended_euclid(r2, r, s2, s, t2, t)
+
+
+def find_d(e, Pn):
+    res = extended_euclid(e, Pn)
+    r = res['s']
+    if r < 0:
+        r = r + Pn
+    return r
+
+
+def gen_prime():
+    N = random.randint(10 ** 127, 10 ** 128 - 1)
+    while (not prime_test(N)):
+        N = N + 1
+    return N
+
+
+def RSA():
+    p = gen_prime()
+    q = gen_prime()
+
+    e = 65537
+
+    Pn = (p - 1) * (q - 1)
+    n = p * q
+
+    d = find_d(e, Pn)
+
+    return [e, d, n]
+
+
+def encryption(a, e, n):
+    return Q_mod(a, e, n)
+
+
+def decryption(enc, d, n):
+    return Q_mod(enc, d, n)
 
 PORT = 9998
 
@@ -22,6 +128,9 @@ def client(self, HOST):
     global LIS
     global REQ
 
+    global C_E
+    global C_N
+
     LIS = True
     REQ = HOST
     s_client.connect((HOST, PORT))
@@ -29,6 +138,14 @@ def client(self, HOST):
 
     if data == 'ALLOW':
         CNT = True
+
+        C_E = int(s_client.recv(1024).decode())
+        C_N = int(s_client.recv(1024).decode())
+
+        self.console.append(f'[클라이언트] 암호화 키 저장')
+        self.console.append(f'[클라이언트] 공개 키 저장')
+
+
     else:
         LIS = False
         s_client.close()
@@ -40,6 +157,9 @@ def server(self):
     global c_server_client
     global CNT
     global s_server
+
+    global S_D
+    global S_N
 
     while (True):
         while (True):
@@ -65,6 +185,16 @@ def server(self):
                 break
 
         CNT = True
+
+        key = RSA()
+        S_D = key[0]
+        S_N = key[2]
+        c_server_client.sendall(str(key[1]).encode())
+        c_server_client.sendall(str(key[2]).encode())
+
+        self.console.append(f'[서버] 암호화 키 전송')
+        self.console.append(f'[서버] 공개 키 전송')
+
         uid = list(ad)[0].split(".")[3]
 
         while (CNT):
